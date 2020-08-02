6f30abed5c704a8fdede99e2d878d4dd64bb13b9
Takafumi Moriyama <taka.youthful.426@gmail.com>
2020-08-02 11:45:36 +0900

    参加者エンティティ_インターフェイス追加

--- src/app/domain/member.go
+++ src/app/domain/member.go
@@ -1,12 +1,38 @@
 package domain
 
 type Member struct {
-    Id        int
-    GroupId int
-    AccountName string
-    role Role
-    voteTo int
-    theme string
+    id        int
+    groupId int
+    accountName string
 }
 
 type Members []Member
+
+func NewMember(id, groupId int, accountName string) *Member {
+	return &Member{
+		id:    id,
+		groupId: groupId,
+		accountName: accountName,
+	}
+}
+
+func (m *Member) GetID() int {
+	return m.id
+}
+
+func (m *Member) GetGroupId() int {
+	return m.groupId
+}
+
+func (m *Member) GetAccountName() string {
+	return m.accountName
+}
+
+// インターフェイス
+type MemberAction interface {
+    JoinGame(id, groupId) error
+    LeaveGame(id, groupId) error
+    StartTalk(id, groupId) error
+    Vote(id, groupId, id) error
+    ConfirmTheme(id, groupId) error
+}
