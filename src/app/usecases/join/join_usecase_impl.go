aa0ccc7b9a3161dd796dcf72ebe5cd50150f17fd
takuya.tominaga <t.tominaga@tcmobile.jp>
2020-11-19 12:44:36 +0900

    修正

--- src/app/usecases/join/join_usecase_impl.go
+++ src/app/usecases/join/join_usecase_impl.go
@@ -3,17 +3,40 @@ package join
 
 import (
 	"github.com/takuyaaaaaaahaaaaaa/go_wordwolf/src/app/domain"
+	"github.com/takuyaaaaaaahaaaaaa/go_wordwolf/src/app/usecases/repository"
 )
 
 type JoinUseCaseImpl struct {
-	gameMasterRepository GameMasterRepository
+	member               domain.Member
+	gameMaster           domain.GameMaster
+	gameMasterRepository repository.GameMasterRepository
+	memberRepository     repository.MemberRepository
 }
 
+// インターフェースを満たしているかのチェック
+var _ JoinUseCase = (*JoinUseCaseImpl)(nil)
+
 // ゲームに参加する
-func (u *JoinUseCaseImpl) joinGame(JoinInput) JoinOutput {
+func (j *JoinUseCaseImpl) JoinGame(input JoinInput) JoinOutput {
 	// ゲームマスターを取得する
+	var gameMaster, err = j.gameMasterRepository.FindGameMasterByGroupID(input.GroupID)
+	if err != nil {
+	}
+	if gameMaster == nil {
+		gameMaster = domain.NewGameMaster(input.GroupID)
+	}
+
+	// メンバーを取得する
+	var ga, err = j.gameMasterRepository.FindGameMasterByGroupID(input.GroupID)
+	if err != nil {
+	}
+	if gameMaster == nil {
+		gameMaster = domain.NewGameMaster(input.GroupID)
+	}
 	// ゲームマスターにメンバーを追加する
 	// メンバーを保存する
 	// ゲームマスタを保存する。
 	// プレゼンターに出力する
+	var output = new(JoinOutput)
+	return *output
 }
