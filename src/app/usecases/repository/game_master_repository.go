46bf83d23a9b0aca9f624d8d13b788acec99c026
takuya.tominaga <t.tominaga@tcmobile.jp>
2020-11-19 14:54:59 +0900

    修正

--- src/app/usecases/repository/game_master_repository.go
+++ src/app/usecases/repository/game_master_repository.go
@@ -9,5 +9,5 @@ type GameMasterRepository interface {
 	// ゲームマスターを保存する
 	Save(domain.GameMaster) error
 	// グループIDを指定してゲームマスターを取得する
-	FindGameMasterByGroupID(string) domain.GameMaster
+	FindGameMasterByGroupID(string) (domain.GameMaster, error)
 }
