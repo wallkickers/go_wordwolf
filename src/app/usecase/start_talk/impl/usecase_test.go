f4af6d4c460b1d9383ab10cca44492f10b685eaa
takuya.tominaga <t.tominaga@tcmobile.jp>
2021-02-04 18:18:05 +0900

    単体テストコメントのリファクタリング

--- src/app/usecase/start_talk/impl/usecase_test.go
+++ src/app/usecase/start_talk/impl/usecase_test.go
@@ -13,34 +13,27 @@ import (
 
 // 正常系
 func TestExecute_withInput_success(t *testing.T) {
-	// ダミーデータ
 	dummyInput := start_talk.Input{
 		ReplyToken:    "testReplyToken",
 		MemberID:      "testMemberID12345",
 		GroupRoomID:   "testGroup12345",
 		GroupRoomType: "group",
 	}
+	dummyGameMaster := domain.NewGameMaster(dummyInput.GroupRoomID, domain.GroupRoomType(dummyInput.GroupRoomType))
 	dummyOutput := start_talk.Output{
 		ReplyToken:    "testReplyToken",
 		MemberID:      "testMemberID12345",
 		GroupRoomID:   "testGroup12345",
 		GroupRoomType: "group",
-		Err:           nil,
+		TalkTimeMin:   dummyGameMaster.TalkTimeMin(), //トーク時間が返却
+		Err:           nil,                           //エラーがnil
 	}
-	dummyGameMaster := domain.NewGameMaster(dummyInput.GroupRoomID, domain.GroupRoomType(dummyInput.GroupRoomType))
-	dummyOutput.TalkTimeMin = dummyGameMaster.TalkTimeMin()
 
-	// 参照用リポジトリMock
 	readOnlyRepositoryMock := new(mocks.ReadOnlyRepository)
 	readOnlyRepositoryMock.On("FindGameMasterByGroupID", dummyInput.GroupRoomID).Return(dummyGameMaster, nil)
-
-	// プレゼンターMock
 	presenterMock := new(mocks.Presenter)
 	presenterMock.On("Execute", dummyOutput).Return(nil)
-	joinUseCase := UseCaseImpl{
-		readOnlyRepository: readOnlyRepositoryMock,
-		presenter:          presenterMock,
-	}
+	joinUseCase := NewUseCaseImpl(readOnlyRepositoryMock, presenterMock)
 
 	// 実行
 	expect := dummyOutput
@@ -55,7 +48,6 @@ func TestExecute_withInput_success(t *testing.T) {
 // 異常系
 // ゲームマスター取得失敗
 func TestExecute_getGM_fail(t *testing.T) {
-	// ダミーデータ
 	dummyInput := start_talk.Input{
 		ReplyToken:    "testReplyToken",
 		MemberID:      "testMemberID12345",
@@ -68,20 +60,14 @@ func TestExecute_getGM_fail(t *testing.T) {
 		MemberID:      "testMemberID12345",
 		GroupRoomID:   "testGroup12345",
 		GroupRoomType: "group",
-		Err:           err,
+		Err:           err, //エラー出力
 	}
 
-	// 参照用リポジトリMock
 	readOnlyRepositoryMock := new(mocks.ReadOnlyRepository)
 	readOnlyRepositoryMock.On("FindGameMasterByGroupID", dummyInput.GroupRoomID).Return(nil, err)
-
-	// プレゼンターMock
 	presenterMock := new(mocks.Presenter)
 	presenterMock.On("Execute", dummyOutput).Return(nil)
-	joinUseCase := UseCaseImpl{
-		readOnlyRepository: readOnlyRepositoryMock,
-		presenter:          presenterMock,
-	}
+	joinUseCase := NewUseCaseImpl(readOnlyRepositoryMock, presenterMock)
 
 	// 実行
 	expect := dummyOutput
@@ -96,7 +82,6 @@ func TestExecute_getGM_fail(t *testing.T) {
 // 異常系
 // 既にトーク中
 func TestExecute_alreadyStart_fail(t *testing.T) {
-	// ダミーデータ
 	dummyInput := start_talk.Input{
 		ReplyToken:    "testReplyToken",
 		MemberID:      "testMemberID12345",
@@ -114,17 +99,11 @@ func TestExecute_alreadyStart_fail(t *testing.T) {
 		Err:           err,
 	}
 
-	// 参照用リポジトリMock
 	readOnlyRepositoryMock := new(mocks.ReadOnlyRepository)
 	readOnlyRepositoryMock.On("FindGameMasterByGroupID", dummyInput.GroupRoomID).Return(dummyGameMaster, nil)
-
-	// プレゼンターMock
 	presenterMock := new(mocks.Presenter)
 	presenterMock.On("Execute", dummyOutput).Return(nil)
-	joinUseCase := UseCaseImpl{
-		readOnlyRepository: readOnlyRepositoryMock,
-		presenter:          presenterMock,
-	}
+	joinUseCase := NewUseCaseImpl(readOnlyRepositoryMock, presenterMock)
 
 	// 実行
 	expect := dummyOutput
@@ -139,7 +118,6 @@ func TestExecute_alreadyStart_fail(t *testing.T) {
 // 正常系
 // 終了タイマー関数の確認
 func TestSetFinishTimer(t *testing.T) {
-	// ダミーデータ
 	dummyInput := start_talk.Input{
 		ReplyToken:    "testReplyToken",
 		MemberID:      "testMemberID12345",
@@ -152,19 +130,13 @@ func TestSetFinishTimer(t *testing.T) {
 		GroupRoomType: "group",
 	}
 
-	// 参照用リポジトリMock
 	readOnlyRepositoryMock := new(mocks.ReadOnlyRepository)
-
-	// プレゼンターMock
 	presenterMock := new(mocks.Presenter)
 	presenterMock.On("FinishTalk", dummyOutput).Return(nil)
-	joinUseCase := UseCaseImpl{
-		readOnlyRepository: readOnlyRepositoryMock,
-		presenter:          presenterMock,
-	}
+	joinUseCase := NewUseCaseImpl(readOnlyRepositoryMock, presenterMock)
 
 	// 実行
-	setFinishTimer(&joinUseCase, dummyInput, time.Second*1)
+	setFinishTimer(joinUseCase, dummyInput, time.Second*1)
 
 	// 検証
 	presenterMock.AssertExpectations(t)
